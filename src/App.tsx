54cb85d1c10393ed5a2c9b5d6b1cfeff465c4494
OLUJOHN2 <olumoyepelumi1@gmail.com>
2026-03-27 22:37:12 +0100

    Note app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NotesApp from "./components/NotesApp";
+
+const App = () => {
+  return (
+    <div>
+      <NotesApp />
+    </div>
+  );
+};
+
+export default App;
