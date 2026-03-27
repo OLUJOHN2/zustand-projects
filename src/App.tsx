a00ae51df1a5d5330b99418aff1d4b621d84b013
OLUJOHN2 <olumoyepelumi1@gmail.com>
2026-03-27 23:46:31 +0100

    Expense Tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NotesApp from "../1. Note App/NotesApp";
 import RecipeApp from "../2. Recipe book/RecipeApp";
+import ExpenseTracker from "../3. Expense tracker/ExpenseTracker";
 
 const App = () => {
   return (
     <div>
       {/* <NotesApp /> */}
-      <RecipeApp />
+      {/* <RecipeApp /> */}
+      <ExpenseTracker />
     </div>
   );
 };
