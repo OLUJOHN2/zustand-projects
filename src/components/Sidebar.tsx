f3d361b97f59e16ef1d8974b55873ec8faf7de5b
OLUJOHN2 <olumoyepelumi1@gmail.com>
2026-03-28 20:53:15 +0100

    Task app

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,60 +1,61 @@
-import { FiSearch } from "react-icons/fi";
-import useNotesStore from "./Store";
+import { FaPlus } from "react-icons/fa";
+import { useStore } from "./Store";
 
 const Sidebar = () => {
-  const { notes, search, selectNote, setSearch } = useNotesStore();
-
-  const filteredNotes = notes.filter((note) =>
-    note.text.toLowerCase().includes(search.toLowerCase()),
-  );
+  const { lists, workspaces, openListModal, openWorkspaceModal } = useStore();
 
   return (
-    <>
-      {/* Sidebar */}
-      <div className="w-1/3 bg-white p-4 shadow-lg">
-        {/* Search Bar */}
-        <div className="flex items-center mb-4">
-          <FiSearch className="text-xl mr-2" />
-          <input
-            type="text"
-            className="w-full border-b focus:outline-none"
-            placeholder="Search notes..."
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-        </div>
-
-        {/* Notes List */}
-        <div>
-          {filteredNotes.length > 0 ? (
-            filteredNotes.map((note, index) => (
-              <div
+    <div className="w-60 bg-[#F9F9F9] flex flex-col">
+      <div className="flex-1 overflow-y-auto">
+        <div className="p-4">
+          <h3 className="text-lg font-semibold flex items-center">Lists</h3>
+          {/* Render Lists */}
+          <ul>
+            {lists.map((list, index) => (
+              <li
                 key={index}
-                className="flex items-center p-4 mb-2 rounded-lg shadow-md cursor-pointer border hover:bg-gray-300"
-                onClick={() => selectNote(index)}
+                className="p-2 hover:bg-[#ccc] rounded-lg
+                 cursor-pointer flex items-center"
               >
-                {/* Color Circle */}
-                <div
-                  className="w-4 h-4 rounded-full mr-2"
-                  style={{
-                    backgroundColor: note.color,
-                    border: "1px solid #000",
-                  }}
-                ></div>
-
-                {/* Note Content */}
-                <div
-                  className="truncate"
-                  dangerouslySetInnerHTML={{ __html: note.text }}
-                />
-              </div>
-            ))
-          ) : (
-            <p>No new notes</p>
-          )}
+                <span className="mr-2">{list.emoji}</span>
+                {list.name}
+              </li>
+            ))}
+          </ul>
+          <button
+            onClick={openListModal}
+            className="flex justify-center items-center mt-[1rem]"
+          >
+            <FaPlus className="mr-2" /> List
+          </button>
+        </div>
+        <div className="p-4">
+          <h3 className="text-lg font-semibold flex items-center">
+            Workspaces
+          </h3>
+          {/* Render Workspaces */}
+          <ul>
+            {workspaces.map((workspace, index) => (
+              <li
+                key={index}
+                className="p-2 hover:bg-[#ccc] rounded-lg 
+                cursor-pointer flex items-center"
+              >
+                <span className="mr-2">{workspace.emoji}</span>
+                {workspace.name}
+              </li>
+            ))}
+          </ul>
+          <button
+            onClick={openWorkspaceModal}
+            className="flex justify-center items-center mt-[1rem]"
+          >
+            <FaPlus className="mr-2" /> Workspace
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
+
 export default Sidebar;
--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -1,72 +1,177 @@
 import { create } from "zustand";
 
-interface Note {
+interface List {
+  name: string;
+  emoji: string;
+}
+
+interface Workspace {
+  name: string;
+  emoji: string;
+}
+
+interface Todo {
   text: string;
-  color: string;
+  list: string;
+  workspace: string;
 }
 
-interface NotesState {
-  notes: Note[];
-  search: string;
-  editorContent: string;
-  noteColor: string;
-  currentNoteIndex: number | null;
-  setNotes: (updatedNotes: Note[]) => void;
-  setSearch: (searchValue: string) => void;
-  setEditorContent: (content: string) => void;
-  setNoteColor: (color: string) => void;
-  setCurrentNoteIndex: (index: number | null) => void;
-  addOrUpdateNote: () => void;
-  selectNote: (index: number) => void;
+interface AppState {
+  lists: List[];
+  workspaces: Workspace[];
+  todos: Todo[];
+  editIndex: number | null;
+  editText: string;
+  dropdownIndex: number | null;
+  isListModalOpen: boolean;
+  isWorkspaceModalOpen: boolean;
+  selectedList: string;
+  selectedWorkspace: string;
+  todoText: string;
+  modalName: string;
+  modalEmoji: string;
+  modalType: "List" | "Workspace" | null;
+  addList: (name: string, emoji: string) => void;
+  addWorkspace: (name: string, emoji: string) => void;
+  addTodo: (todo: Todo) => void;
+  updateTodo: (index: number, updatedTodo: Todo) => void;
+  deleteTodo: (index: number) => void;
+  handleEdit: (index: number) => void;
+  handleUpdate: (index: number) => void;
+  handleDropdownClick: (index: number) => void;
+  setEditText: (text: string) => void;
+  setEditIndex: (index: number | null) => void;
+  openListModal: () => void;
+  closeListModal: () => void;
+  openWorkspaceModal: () => void;
+  closeWorkspaceModal: () => void;
+  setSelectedList: (list: string) => void;
+  setSelectedWorkspace: (workspace: string) => void;
+  setTodoText: (text: string) => void;
+  handleAddTodo: () => void;
+  setModalName: (name: string) => void;
+  setModalEmoji: (emoji: string) => void;
+  setModalType: (type: "List" | "Workspace") => void;
+  handleSaveModal: () => void;
 }
 
-const useNotesStore = create<NotesState>((set) => ({
-  notes: [],
-  search: "",
-  editorContent: "",
-  noteColor: "#ffffff",
-  currentNoteIndex: null,
-
-  setNotes: (updatedNotes) => set(() => ({ notes: updatedNotes })),
-  setSearch: (searchValue) => set(() => ({ search: searchValue })),
-  setEditorContent: (content) => set(() => ({ editorContent: content })),
-  setNoteColor: (color) => set(() => ({ noteColor: color })),
-  setCurrentNoteIndex: (index) => set(() => ({ currentNoteIndex: index })),
-
-  addOrUpdateNote: () =>
-    set((state) => {
-      const { editorContent, noteColor, currentNoteIndex, notes } = state;
-      if (editorContent.trim()) {
-        if (currentNoteIndex !== null) {
-          // Update existing note
-          const updatedNotes = [...notes];
-          updatedNotes[currentNoteIndex] = {
-            text: editorContent,
-            color: noteColor,
-          };
-          return {
-            notes: updatedNotes,
-            editorContent: "",
-            noteColor: "#ffffff",
-            currentNoteIndex: null,
-          };
-        } else {
-          return {
-            notes: [...notes, { text: editorContent, color: noteColor }],
-            editorContent: "",
-            noteColor: "#ffffff",
-            currentNoteIndex: null,
-          };
-        }
-      }
-    }),
-
-  selectNote: (index) =>
+export const useStore = create<AppState>((set) => ({
+  lists: [],
+  workspaces: [],
+  todos: [],
+  editIndex: null,
+  editText: "",
+  dropdownIndex: null,
+  isListModalOpen: false,
+  isWorkspaceModalOpen: false,
+  selectedList: "",
+  selectedWorkspace: "",
+  todoText: "",
+  modalName: "",
+  modalEmoji: "",
+  modalType: null,
+  addList: (name, emoji) =>
     set((state) => ({
-      currentNoteIndex: index,
-      editorContent: state.notes[index].text,
-      noteColor: state.notes[index].color,
+      lists: [...state.lists, { name, emoji }],
     })),
-}));
+  addWorkspace: (name, emoji) =>
+    set((state) => ({
+      workspaces: [...state.workspaces, { name, emoji }],
+    })),
+  addTodo: (todo) =>
+    set((state) => ({
+      todos: [...state.todos, todo],
+    })),
+  updateTodo: (index, updatedTodo) =>
+    set((state) => {
+      const newTodos = [...state.todos];
+      newTodos[index] = updatedTodo;
+      return {
+        todos: newTodos,
+        editIndex: null,
+        editText: "",
+      };
+    }),
+  deleteTodo: (index) =>
+    set((state) => ({
+      todos: state.todos.filter((_, i) => i !== index),
+      dropdownIndex: null,
+    })),
+  handleEdit: (index) =>
+    set((state) => ({
+      editIndex: index,
+      editText: state.todos[index].text,
+      dropdownIndex: null,
+    })),
+  handleUpdate: (index) =>
+    set((state) => {
+      const updatedTodo = {
+        ...state.todos[index],
+        text: state.editText,
+      };
+      const newTodos = [...state.todos];
+      newTodos[index] = updatedTodo;
+      return {
+        todos: newTodos,
+        editIndex: null,
+        editText: "",
+      };
+    }),
+  handleDropdownClick: (index) =>
+    set((state) => ({
+      dropdownIndex: index === state.dropdownIndex ? null : index,
+    })),
+  setEditText: (text) => set({ editText: text }),
+  setEditIndex: (index) => set({ editIndex: index }),
+  openListModal: () => set({ isListModalOpen: true, modalType: "List" }),
+  closeListModal: () =>
+    set({ isListModalOpen: false, modalName: "", modalEmoji: "" }),
+  openWorkspaceModal: () =>
+    set({ isWorkspaceModalOpen: true, modalType: "Workspace" }),
+  closeWorkspaceModal: () =>
+    set({ isWorkspaceModalOpen: false, modalName: "", modalEmoji: "" }),
+  setSelectedList: (list) => set({ selectedList: list }),
+  setSelectedWorkspace: (workspace) => set({ selectedWorkspace: workspace }),
+  setTodoText: (text) => set({ todoText: text }),
+  handleAddTodo: () =>
+    set((state) => {
+      const { todoText, selectedList, selectedWorkspace } = state;
+      if (todoText.trim() === "") {
+        alert("Todo cannot be empty");
+        return state;
+      }
+      const newTodo = {
+        text: todoText,
+        list: selectedList,
+        workspace: selectedWorkspace,
+      };
+      return {
+        todos: [...state.todos, newTodo],
+        todoText: "",
+        selectedList: "",
+        selectedWorkspace: "",
+      };
+    }),
+  setModalName: (name) => set({ modalName: name }),
+  setModalEmoji: (emoji) => set({ modalEmoji: emoji }),
+  setModalType: (type) => set({ modalType: type }),
+  handleSaveModal: () =>
+    set((state) => {
+      const { modalName, modalEmoji, modalType } = state;
+      if (modalName.trim() === "") return state;
 
-export default useNotesStore;
+      if (modalType === "List") {
+        state.addList(modalName, modalEmoji);
+      } else if (modalType === "Workspace") {
+        state.addWorkspace(modalName, modalEmoji);
+      }
+
+      return {
+        modalName: "",
+        modalEmoji: "",
+        modalType: null,
+        isListModalOpen: false,
+        isWorkspaceModalOpen: false,
+      };
+    }),
+}));
