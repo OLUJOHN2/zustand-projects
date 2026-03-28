8d7726692888a726a8f30b158f71111134a66882
OLUJOHN2 <olumoyepelumi1@gmail.com>
2026-03-28 09:53:31 +0100

    Meal project

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import NotesApp from "../1. Note App/NotesApp";
 import RecipeApp from "../2. Recipe book/RecipeApp";
 import ExpenseTracker from "../3. Expense tracker/ExpenseTracker";
+import Meals from "../Meals project/Meals";
 
 const App = () => {
   return (
     <div>
       {/* <NotesApp /> */}
       {/* <RecipeApp /> */}
-      <ExpenseTracker />
+      {/* <ExpenseTracker /> */}
+      <Meals />
     </div>
   );
 };
